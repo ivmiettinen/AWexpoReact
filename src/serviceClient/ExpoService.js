26c808e36c3cb0ebba466064098cdc9b2b04e0cd
Ilkka <55871738+ivmiettinen@users.noreply.github.com>
2021-03-16 17:13:15 +0200

    cleaning code

--- src/serviceClient/ExpoService.js
+++ src/serviceClient/ExpoService.js
@@ -1,6 +1,6 @@
 const url = 'http://api.academicwork.net/api/adverts?country=fi';
 
-// const LocalProxyURL = 'http://localhost:8080/';
+
 
 const herokuProxyUrl = 'https://corsanywherenodejs.herokuapp.com/';
 
