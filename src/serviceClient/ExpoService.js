eeb3d553403b644e07b2ba454e2fef45f39fd783
Ilkka <55871738+ivmiettinen@users.noreply.github.com>
2021-03-16 16:23:15 +0200

    updated cors and start methods

--- src/serviceClient/ExpoService.js
+++ src/serviceClient/ExpoService.js
@@ -2,7 +2,7 @@ const url = 'http://api.academicwork.net/api/adverts?country=fi';
 
 // const LocalProxyURL = 'http://localhost:8080/';
 
-const herokuProxyUrl = 'https://cors-anywhere.herokuapp.com/';
+const herokuProxyUrl = 'https://corsanywherenodejs.herokuapp.com/';
 
 export const getWorkData = async () => {
   return fetch(herokuProxyUrl + url).then(resp => resp.json());
